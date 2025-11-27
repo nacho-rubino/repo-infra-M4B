301b2f0be88fe6f9ceeb41336b1ad35b2f39ebeb
Nacho <ignaciorubino14@gmail.com>
2025-11-27 18:30:47 -0300

    arrelos 2

--- tambo.adb
+++ tambo.adb
@@ -1,232 +1,255 @@
-with Ada.Text_IO;               use Ada.Text_IO;
+with Ada.Text_IO;           use Ada.Text_IO;
 with Ada.Numerics.Discrete_Random;
+with Ada.Integer_Text_IO;  use Ada.Integer_Text_IO;
+with Ada.Calendar;
+with Ada.Real_Time;        use Ada.Real_Time;
 
 procedure Tambo is
+   -------------------------------------------------------------------------
+   -- CONFIG
+   -------------------------------------------------------------------------
+   Num_Cows     : constant Positive := 100;
+   Cap_Milking  : constant Positive := 15;  -- capacidad sala de ordenie
+   Cap_Vacc     : constant Positive := 5;   -- capacidad area vacunacion
+   Cap_Truck    : constant Positive := 50;  -- capacidad por camion
 
-   -- Configuracion
-
-   Cantidad_Vacas     : constant Integer := 100;
-   Capacidad_Ordenie  : constant Integer := 15;
-   Capacidad_Vacunas  : constant Integer := 5;
-   Capacidad_Camion   : constant Integer := 50;
-
-   subtype Rango_Azar is Integer range 0 .. 1000;
-   package Azar_Entero is new Ada.Numerics.Discrete_Random(Rango_Azar);
-   Generador_Global : Azar_Entero.Generator;
-
-
-   function Tiempo_Aleatorio (Max : Float) return Duration is
-      Valor : Rango_Azar := Azar_Entero.Random(Generador_Global);
-   begin
-      return Duration (Float(Valor) / 1000.0 * Max);
-   end Tiempo_Aleatorio;
-
-
-   -- pasillo (UNA vaca a la vez)
-
-   protected Pasillo is
-      entry Entrar (Vaca_Id : Integer);
-      entry Salir  (Vaca_Id : Integer);
+   -------------------------------------------------------------------------
+   -- LOG protegido para ordenar todas las salidas en pantalla
+   -------------------------------------------------------------------------
+   protected Log is
+      procedure Print (S : String);
    private
-      Ocupado : Boolean := False;
+   end Log;
+
+   protected body Log is
+      procedure Print (S : String) is
+      begin
+         Put_Line (S);
+      end Print;
+   end Log;
+
+   -------------------------------------------------------------------------
+   -- PASILLO (solo 1 vaca a la vez)
+   -- Secuencia que deben seguir las vacas:
+   --   1) Pasillo.Enter (se imprime "entrando al pasillo")
+   --   2) Pasillo.Leave (se imprime "saliendo del pasillo")
+   --   3) Entrar al area (Milking.Enter o Vacc.Enter) -> imprime "entrando al area..."
+   -------------------------------------------------------------------------
+   protected Pasillo is
+      entry Enter_Pasillo (Id : Positive);
+      entry Leave_Pasillo (Id : Positive);
+   private
+      Busy : Boolean := False;
    end Pasillo;
 
    protected body Pasillo is
-
-      entry Entrar (Vaca_Id : Integer)
-        when not Ocupado
+      entry Enter_Pasillo (Id : Positive)
+        when not Busy
       is
       begin
-         Ocupado := True;
-         Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                   " está entrando al pasillo");
-      end Entrar;
+         Busy := True;
+         Log.Print ("La vaca " & Id'Image & " esta entrando al pasillo");
+      end Enter_Pasillo;
 
-      entry Salir (Vaca_Id : Integer)
-        when Ocupado
+      entry Leave_Pasillo (Id : Positive)
+        when Busy
       is
       begin
-         Ocupado := False;
-         Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                   " está saliendo del pasillo");
-      end Salir;
-
+         -- la vaca libera el pasillo
+         Busy := False;
+         Log.Print ("La vaca " & Id'Image & " esta saliendo del pasillo");
+      end Leave_Pasillo;
    end Pasillo;
 
-
-   -- Area de ordenie (max 15 vacas)
-   protected Ordenie is
-      entry Entrar (Vaca_Id : Integer);
-      entry Salir  (Vaca_Id : Integer);
+   -------------------------------------------------------------------------
+   -- AREA DE ORDENIE (semáforo con capacidad Cap_Milking)
+   -------------------------------------------------------------------------
+   protected Milking is
+      entry Enter_Milking (Id : Positive);
+      entry Leave_Milking (Id : Positive);
+      function Occupied return Integer;
    private
-      Ocupacion : Integer := 0;
-   end Ordenie;
+      Count : Integer := 0;
+   end Milking;
 
-   protected body Ordenie is
-
-      entry Entrar (Vaca_Id : Integer)
-        when Ocupacion < Capacidad_Ordenie
+   protected body Milking is
+      entry Enter_Milking (Id : Positive)
+        when Count < Cap_Milking
       is
       begin
-         Ocupacion := Ocupacion + 1;
-         Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                   " está entrando al área de ordeñe");
-      end Entrar;
+         Count := Count + 1;
+         Log.Print ("La vaca " & Id'Image & " esta entrando al area de ordenie");
+      end Enter_Milking;
 
-      entry Salir (Vaca_Id : Integer)
-        when Ocupacion > 0
+      entry Leave_Milking (Id : Positive)
+        when Count > 0
       is
       begin
-         Ocupacion := Ocupacion - 1;
-         Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                   " está saliendo del área de ordeñe");
-      end Salir;
+         Count := Count - 1;
+         Log.Print ("La vaca " & Id'Image & " esta saliendo del area de ordenie");
+      end Leave_Milking;
 
-   end Ordenie;
+      function Occupied return Integer is
+      begin
+         return Count;
+      end Occupied;
+   end Milking;
 
-
-   -- Area de vac (máx 5 vacas - FIFO de mangas)
-
-
-   protected Vacunacion is
-      entry Solicitar_Entrada (Vaca_Id : Integer);
-      entry Entrar_Vacuna     (Vaca_Id : Integer);
-      entry Salir_Vacuna      (Vaca_Id : Integer);
+   -------------------------------------------------------------------------
+   -- AREA DE VACUNACION (semáforo con capacidad Cap_Vacc)
+   -------------------------------------------------------------------------
+   protected Vaccination is
+      entry Enter_Vacc (Id : Positive);
+      entry Leave_Vacc (Id : Positive);
+      function Occupied return Integer;
    private
-      Ocupacion : Integer := 0;
-   end Vacunacion;
+      Count : Integer := 0;
+   end Vaccination;
 
-   protected body Vacunacion is
-
-      entry Solicitar_Entrada (Vaca_Id : Integer)
-        when Ocupacion < Capacidad_Vacunas
+   protected body Vaccination is
+      entry Enter_Vacc (Id : Positive)
+        when Count < Cap_Vacc
       is
       begin
-         null;
-      end Solicitar_Entrada;
+         Count := Count + 1;
+         Log.Print ("La vaca " & Id'Image & " esta entrando al area de vacunacion");
+      end Enter_Vacc;
 
-      entry Entrar_Vacuna (Vaca_Id : Integer)
-        when Ocupacion < Capacidad_Vacunas
+      entry Leave_Vacc (Id : Positive)
+        when Count > 0
       is
       begin
-         Ocupacion := Ocupacion + 1;
-         Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                   " está entrando al área de vacunación");
-      end Entrar_Vacuna;
+         Count := Count - 1;
+         Log.Print ("La vaca " & Id'Image & " esta saliendo del area de vacunacion");
+      end Leave_Vacc;
 
-      entry Salir_Vacuna (Vaca_Id : Integer)
-        when Ocupacion > 0
-      is
+      function Occupied return Integer is
       begin
-         Ocupacion := Ocupacion - 1;
-         Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                   " está saliendo del área de vacunación");
-      end Salir_Vacuna;
+         return Count;
+      end Occupied;
+   end Vaccination;
 
-   end Vacunacion;
-
-
-   -- Camiones
-
-   protected Camiones is
-      entry Subir (Vaca_Id : Integer);
+   -------------------------------------------------------------------------
+   -- CAMIONES: dos camiones con capacidad Cap_Truck cada uno
+   -------------------------------------------------------------------------
+   protected Trucks is
+      entry Board (Id : Positive);
+      function Total_Load return Integer;
    private
-      Camion1 : Integer := 0;
-      Camion2 : Integer := 0;
-   end Camiones;
+      T1 : Integer := 0;
+      T2 : Integer := 0;
+   end Trucks;
 
-   protected body Camiones is
-
-      entry Subir (Vaca_Id : Integer)
-        when (Camion1 < Capacidad_Camion) or else (Camion2 < Capacidad_Camion)
+   protected body Trucks is
+      entry Board (Id : Positive)
+        when (T1 < Cap_Truck) or (T2 < Cap_Truck)
       is
       begin
-         if Camion1 < Capacidad_Camion then
-            Camion1 := Camion1 + 1;
-            Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                      " está subiendo al Camión 1");
+         if T1 < Cap_Truck then
+            T1 := T1 + 1;
+            Log.Print ("La vaca " & Id'Image & " esta subiendo al Camion 1");
          else
-            Camion2 := Camion2 + 1;
-            Put_Line ("La vaca" & Integer'Image(Vaca_Id) &
-                      " está subiendo al Camión 2");
+            T2 := T2 + 1;
+            Log.Print ("La vaca " & Id'Image & " esta subiendo al Camion 2");
          end if;
-      end Subir;
+      end Board;
 
-   end Camiones;
+      function Total_Load return Integer is
+      begin
+         return T1 + T2;
+      end Total_Load;
+   end Trucks;
 
+   -------------------------------------------------------------------------
+   -- GENERADOR ALEATORIO (Discrete_Random)
+   -------------------------------------------------------------------------
+   subtype Rand_Range is Integer range 0 .. 1000;
+   package Rand_Int is new Ada.Numerics.Discrete_Random (Rand_Range);
+   Gen : Rand_Int.Generator;
 
-   -- Task VACA!!!!
-   -- acaaaaaaaaaaaa
-
-
-   task type Vaca (Identificador : Integer);
-
-   task body Vaca is
-      Valor_Azar     : Rango_Azar := Azar_Entero.Random(Generador_Global);
-      Primero_Vacuna : Boolean := (Valor_Azar mod 2 = 0);
+   function Rand_Time (Max : Float) return Duration is
+      R : Rand_Range := Rand_Int.Random(Gen);
    begin
+      return Duration (Float(R) / 1000.0 * Max);
+   end Rand_Time;
 
-      if Primero_Vacuna then
-         -- Pasillo - Vac
-         Pasillo.Entrar(Identificador);
-         Vacunacion.Solicitar_Entrada(Identificador);
-         Vacunacion.Entrar_Vacuna(Identificador);
-         Pasillo.Salir(Identificador);
+   -------------------------------------------------------------------------
+   -- TASK VACA
+   -------------------------------------------------------------------------
+   task type Cow (Id : Positive);
 
-         delay Tiempo_Aleatorio(2.0);
+   task body Cow is
+      -- Decide una vez si vacuna primero
+      R : Rand_Range := Rand_Int.Random(Gen);
+      Vacc_First : Boolean := (R mod 2 = 0);
+   begin
+      -- Realizamos el movimiento por pasillo ANTES de entrar al area
+      if Vacc_First then
+         -- Pasillo -> Vacunacion -> Ordeñe
+         Pasillo.Enter_Pasillo (Id);
+         -- simulamos movimiento por pasillo (pequeña demora)
+         delay 0.001;
+         Pasillo.Leave_Pasillo (Id);
 
-         Vacunacion.Salir_Vacuna(Identificador);
+         -- ahora intentar entrar al area de vacunacion (si esta llena espera)
+         Vaccination.Enter_Vacc (Id);
+         delay Rand_Time (1.5);  -- tiempo de vacunacion aleatorio
+         Vaccination.Leave_Vacc (Id);
 
-         -- pasillo - Ordenie
+         -- luego pasillo otra vez para moverse a ordenie
+         Pasillo.Enter_Pasillo (Id);
+         delay 0.001;
+         Pasillo.Leave_Pasillo (Id);
 
-         Pasillo.Entrar(Identificador);
-         Ordenie.Entrar(Identificador);
-         Pasillo.Salir(Identificador);
-
-         delay Tiempo_Aleatorio(3.0);
-
-         Ordenie.Salir(Identificador);
+         Milking.Enter_Milking (Id);
+         delay Rand_Time (2.5);  -- tiempo de ordenie aleatorio
+         Milking.Leave_Milking (Id);
 
       else
-         -- Pasillo - Ordenie
+         -- Pasillo -> Ordeñe -> Vacunacion
+         Pasillo.Enter_Pasillo (Id);
+         delay 0.001;
+         Pasillo.Leave_Pasillo (Id);
 
-         Pasillo.Entrar(Identificador);
-         Ordenie.Entrar(Identificador);
-         Pasillo.Salir(Identificador);
+         Milking.Enter_Milking (Id);
+         delay Rand_Time (2.5);
+         Milking.Leave_Milking (Id);
 
-         delay Tiempo_Aleatorio(3.0);
+         Pasillo.Enter_Pasillo (Id);
+         delay 0.001;
+         Pasillo.Leave_Pasillo (Id);
 
-         Ordenie.Salir(Identificador);
-
-         -- PASILLO - VACUNACIÓN
-
-         Pasillo.Entrar(Identificador);
-         Vacunacion.Solicitar_Entrada(Identificador);
-         Vacunacion.Entrar_Vacuna(Identificador);
-         Pasillo.Salir(Identificador);
-
-         delay Tiempo_Aleatorio(2.0);
-
-         Vacunacion.Salir_Vacuna(Identificador);
+         Vaccination.Enter_Vacc (Id);
+         delay Rand_Time (1.5);
+         Vaccination.Leave_Vacc (Id);
       end if;
 
-      -- Subir camion
+      -- Finalmente, ir al embarque (camiones)
+      Trucks.Board (Id);
+   end Cow;
 
-      Camiones.Subir(Identificador);
-
-   end Vaca;
-
-
-   -- Crear 100  vacas
-
-   type Acceso_Vaca is access Vaca;
-   Vacas : array (1 .. Cantidad_Vacas) of Acceso_Vaca;
+   -------------------------------------------------------------------------
+   -- CREACION DINAMICA DE LAS VACAS (evita problemas con arrays de tasks)
+   -------------------------------------------------------------------------
+   type Cow_Access is access Cow;
+   Cows : array (1 .. Num_Cows) of Cow_Access;
 
 begin
-   Azar_Entero.Reset(Generador_Global);
+   -- Inicializar RNG
+   Rand_Int.Reset (Gen);
 
-   for I in Vacas'Range loop
-      Vacas(I) := new Vaca(I);
+   -- Crear y lanzar las tasks vaca
+   for I in Cows'Range loop
+      Cows (I) := new Cow (I);
    end loop;
 
+   -- Esperar hasta que todos terminen (simplemente esperar que todos se hayan subido)
+   -- Una forma simple: busy-wait hasta que Trucks.Total_Load = Num_Cows
+   loop
+      exit when Trucks.Total_Load = Num_Cows;
+      delay 0.1;
+   end loop;
+
+   Log.Print ("Simulacion finalizada: total vacas en camiones = " &
+              Integer'Image (Trucks.Total_Load));
 end Tambo;
